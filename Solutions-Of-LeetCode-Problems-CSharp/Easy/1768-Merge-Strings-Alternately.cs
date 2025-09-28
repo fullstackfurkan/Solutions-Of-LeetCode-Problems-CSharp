17ea24dfcc77d172c11352377f5f6520f91ddc44
Furkan Yılmaz <furkanyilmaz870@gmail.com>
2025-09-29 01:33:47 +0300

    Applied latest changes

---
+++ Solutions-Of-LeetCode-Problems-CSharp/Easy/1768-Merge-Strings-Alternately.cs
@@ -0,0 +1,27 @@
+﻿
+namespace Solutions_Of_LeetCode_Problems_CSharp.Easy
+{
+    public class _1768_Merge_Strings_Alternately
+    {
+        public string MergeAlternately(string word1, string word2)
+        {
+            string newString = "";
+            int shortestWordLength = word1.Length < word2.Length ? word1.Length : word2.Length;
+            string longestWord = word1.Length > word2.Length ? word1 : word2;
+            int letterDifferenceCount = longestWord.Length - shortestWordLength;
+
+            for (int i = 0; shortestWordLength > i; i++)
+                newString += $"{word1[i]}{word2[i]}";
+
+            if(word1.Length != word2.Length)
+            {
+
+            }
+            //newString += $"{longestWord.Substring(letterDifferenceCount)}";
+
+            Console.WriteLine(newString);
+
+            return newString;
+        }
+    }
+}
---
+++ Solutions-Of-LeetCode-Problems-CSharp/Program.cs
@@ -0,0 +1,14 @@
+﻿using Solutions_Of_LeetCode_Problems_CSharp.Easy;
+
+namespace Solutions_Of_LeetCode_Problems_CSharp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            _1768_Merge_Strings_Alternately exampleClass = new();
+
+            exampleClass.MergeAlternately("cfd", "a");
+        }
+    }
+}
